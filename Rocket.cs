e4e43ca0d4f740a740bb02ec27ea52c68ec0e390
SHARK5759 <121607662+SHARK5759@users.noreply.github.com>
2023-01-12 09:49:17 +0800

    Update Rocket.cs
    访问游戏中火箭的Rigidbody，并进入Rigidbody

--- Rocket.cs
+++ Rocket.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>(); //访问游戏中的火箭的Rigidbody，并进入Rigidbody
     }
 
     // Update is called once per frame
@@ -20,15 +21,15 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Trusting"); 
+            rigidbody.AddRelativeForce(Vector3.up);
         }
         if(Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            rigidbody.AddRelativeForce(Vector3.left);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating right");
+            rigidbody.AddRelativeForce(Vector3.right);
         }
 
 
