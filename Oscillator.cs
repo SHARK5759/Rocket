29eeac4054643319276f9f606ae40e4cfbb1f016
SHARK5759 <121607662+SHARK5759@users.noreply.github.com>
2023-01-14 13:23:16 +0800

    Update Oscillator.cs
    增加了一个移动的物体，运用了数学知识，很难

--- Oscillator.cs
+++ Oscillator.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
+
     [Range(0, 1)][SerializeField]float movementFactor; //0 for not move,1 for fully move
     Vector3 startingPos;
     void Start()
     {
-        startingPos = transform.position;//ªÒµ√∏√Ω®÷˛ŒÔµƒŒª÷√
+        startingPos = transform.position;//Ëé∑ÂæóËØ•Âª∫Á≠ëÁâ©ÁöÑ‰ΩçÁΩÆ
     }
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;// grows continuely from 0
+
+        const float tau = Mathf.PI * 2; //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);//goes from -1 to 1;
+        movementFactor = rawSinWave / 2f+0.5f;
         Vector3 offset = movementFactor * movementVector;
         transform.position = startingPos + offset;
     }
